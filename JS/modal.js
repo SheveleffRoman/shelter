4c736b68ba95fc062c04497953cea24d17fd6bc5
SheveleffRoman <118383206+SheveleffRoman@users.noreply.github.com>
2023-03-23 14:25:39 +0400

    fix: change function

--- JS/modal.js
+++ JS/modal.js
@@ -4,23 +4,45 @@ const blackout = document.querySelector('div.blackout');
 const modalWindow = document.querySelector('div.modal-window');
 const body = document.body;
 
-// для каждой карточки мы ставим слушатель клика
 
-// cards.forEach( elem => elem.addEventListener('click', () => {
-//     blackout.classList.add('view');
-//     modalWindowContainer.classList.add('modalView');
-//     body.classList.add('noScroll');
-//     changeCardInfo(elem);
-// }));
+function cardClick() {
+    for (i=0;i<cards.length;i++) {
+        cards[i].onclick = function () {
+            getPopup();
+            let index = this.id - 1;
+            updateModalInfo(index);
+        }
+    }
+}
 
-cards.forEach((elem) => {
-    elem.addEventListener('click', () => {
-        blackout.classList.add('view');
-        modalWindowContainer.classList.add('modalView');
-        body.classList.add('noScroll');
-        changeCardInfo(elem);
-    })
-})
+cardClick();
+
+function updateModalInfo(i) {
+    modalWindow.innerHTML = `
+    <img class='main-pic' src="${pets_array[i].img}">
+     <div class="modal-content">
+         <h2>${pets_array[i].name}</h2>
+                 <h3>${pets_array[i].type} - ${pets_array[i].breed}</h3>
+                 <h5>${pets_array[i].description}</h5>
+                 <ul>
+                     <li>Age: ${pets_array[i].age}</li>
+                     <li>Inoculations: ${pets_array[i].inoculations}</li>
+                     <li>Diseases: ${pets_array[i].diseases}</li>
+                     <li>Parasites: ${pets_array[i].parasites}</li>
+                 </ul>
+             </div>`;
+}
+
+function getPopup() {
+    getModal();
+    modalWindow.classList.toggle('modalView');
+}
+
+function getModal() {
+    blackout.classList.add('view');
+    modalWindowContainer.classList.add('modalView');
+    body.classList.add('noScroll');
+}
 
 modalCloseBtn.addEventListener('click', () => {
     blackout.classList.remove('view');
@@ -33,25 +55,3 @@ blackout.addEventListener('click', () => {
     modalWindowContainer.classList.remove('modalView');
     body.classList.remove('noScroll');
 })
-
-
-// функция для замены модульного окна
-function changeCardInfo(elem) {
-    let i = elem.id - 1;
-    console.log(i);
-    modalWindow.innerHTML = `
-    <img class='main-pic' src="${pets_array[i].img}">
-    <div class="modal-content">
-        <h2>${pets_array[i].name}</h2>
-                <h3>${pets_array[i].type} - ${pets_array[i].breed}</h3>
-                <h5>${pets_array[i].description}</h5>
-                <ul>
-                    <li>Age: ${pets_array[i].age}</li>
-                    <li>Inoculations: ${pets_array[i].inoculations}</li>
-                    <li>Diseases: ${pets_array[i].diseases}</li>
-                    <li>Parasites: ${pets_array[i].parasites}</li>
-                </ul>
-            </div>
-        </div>
-    </div>`
-};
